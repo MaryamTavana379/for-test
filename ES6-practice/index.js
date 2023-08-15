962f9ce2ec69adac500d80ff38b461d3cad2685e
MaryamTavana379 <maryam.tavana379@gmail.com>
2023-08-15 20:02:18 +0330

    map

--- ES6-practice/index.js
+++ ES6-practice/index.js
@@ -1,3 +1,26 @@
+//Num1
 const arr = [1,5,6,8,10]
-const output = arr.map(item => item + 1)
-console.log(output)
+const MyFunc = ()=>{
+    return(
+        arr.map(item => item+1)
+    )
+}
+// arr = [56,11] //We can't do this
+// console.log(arr)
+ console.log(MyFunc());
+
+//Num2
+ let type = "123" , color = "red" ;
+ var obj = {type: type, color: color}
+ console.log(obj);
+//  type = "456" //The value changed
+//  console.log(type)
+
+ //Num3
+ let type1 = "456" , color1 = "blue";
+ var obj1 = {type1, color1}
+ console.log(obj1) 
+//  type1 = "888" //Thr value changed
+//  console.log(type1)
+
+
