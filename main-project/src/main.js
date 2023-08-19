f03d302aeaefccd62d9e7072b6727b47e6855878
MaryamTavana379 <maryam.tavana379@gmail.com>
2023-08-19 20:05:29 +0330

    added-font

--- main-project/src/main.js
+++ main-project/src/main.js
@@ -1,1 +1,12 @@
-import './assets/styles/home.scss'
+import "./assets/styles/home.scss";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Swiper from "swiper/bundle";
+
+new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
