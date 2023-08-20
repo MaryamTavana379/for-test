525f9d735089910b8da6f127accf476b850add56
MaryamTavana379 <maryam.tavana379@gmail.com>
2023-08-21 00:54:32 +0330

    exhibition-section

--- main-project/src/main.js
+++ main-project/src/main.js
@@ -1,8 +1,8 @@
 import "./assets/styles/home.scss";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css";
 import Swiper from "swiper/bundle";
 
 new Swiper(".mySwiper", {
@@ -11,14 +11,43 @@ new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
- new Swiper(".product__swiper", {
+new Swiper(".product__swiper", {
   slidesPerView: 4,
-  spaceBetween: 20,
-  
+  spaceBetween: 30,
+  centeredSlides: true,
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
- 
+});
+new Swiper(".exhibition__swiper", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 100,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    1500: {
+      slidesPerView: 1.5,
+      spaceBetween: 30,
+    },
+  },
 });
