fa32a0b0543895e31913b20fd83ca5ea86604368
MaryamTavana379 <maryam.tavana379@gmail.com>
2023-08-20 14:20:48 +0330

    add-header-products

--- main-project/src/main.js
+++ main-project/src/main.js
@@ -2,11 +2,20 @@ import "./assets/styles/home.scss";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css';
 import Swiper from "swiper/bundle";
 
 new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+ new Swiper(".mySwiper1", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
