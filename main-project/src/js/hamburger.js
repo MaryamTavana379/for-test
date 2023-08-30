a887e80003275309c25e80e57290428f7e2b4d28
MaryamTavana379 <maryam.tavana379@gmail.com>
2023-08-30 21:07:20 +0330

    change-font-size

--- main-project/src/js/hamburger.js
+++ main-project/src/js/hamburger.js
@@ -1,18 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav__list--right");
-// let divtag = document.createElement("div");
-const body = document.getElementById("#body");
-// const x = document.getElementById("nav__list--right")
-// if(navMenu.style.position == "fixed"){
+const html = document.getElementsByTagName("html")[0];
 
- 
-// }
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-  body.style.overflowY = "hidden"
-//   document.getElementById("navigation").appendChild(divtag)
- 
+  html.style.overflowY = "hidden";
+  //   document.getElementById("navigation").appendChild(divtag)
 }
