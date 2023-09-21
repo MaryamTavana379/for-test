a95617dcc0c032735f24270d84d7d93ea688d6a5
MaryamTavana379 <maryam.tavana379@gmail.com>
2023-09-21 14:26:00 +0330

    new practice

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Reaction timer</h1>
+    <button @click="start" :disabled="isplaying">start</button>
+    <Block v-if="isplaying" :delay="delay" />
+  </div>
+</template>
+
+
+<script >
+import Block from './components/Block.vue';
+export default {
+  name: 'App',
+  components: {Block},
+  data(){
+    return{
+      delay: null,
+      isplaying: false,
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000+ Math.random()*5000;
+      this.isplaying = !this.isplaying
+    }
+  }
+
+};
+</script>
+
+<style >
+body{
+  text-align: center;
+}
+</style>
---
+++ reaction-timer/src/components/Block.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block">Click me !</div>
+</template>
+
+<script>
+export default {
+    props:["delay"]
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  margin: 40px auto;
+  padding: 100px;
+  text-align: center;
+  background-color: #0faf87;
+  color: white;
+}
+</style>
